133116d9c2873991c7d973d844ea25aefe4aacb4
Sebastian Linero De Castro <136842642+SlineroDC@users.noreply.github.com>
2025-12-29 21:24:19 -0500

    fix: delete logos de api que no quiero

--- api/Services/StackService.cs
+++ api/Services/StackService.cs
@@ -11,14 +11,12 @@ public class StackService
             new("Vue.js", "vue", "frontend"),
             new("Tailwind", "tailwind", "frontend"),
             new("JavaScript", "javascript", "frontend"),
-            new("HTML5", "html", "frontend"),
             // Backend
             new(".NET Core", "dotnet", "backend"),
             new("C#", "csharp", "backend"),
             new("Node.js", "node", "backend"),
             new("Python", "python", "backend"),
             // Database
-            new("SQL Server", "sql", "database"),
             new("MySQL", "mysql", "database"),
             new("MongoDB", "mongo", "database"),
             // Tools
